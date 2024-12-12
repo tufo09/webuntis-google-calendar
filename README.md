c41410310937f2b5d60597fccf55770f13623f77
tufo09 <144875509+tufo09@users.noreply.github.com>
2024-12-12 06:37:54 +0100

    doing tests with the api, figuring out the layout of the response; have added a README file which has an example for a configuration.py file

---
+++ README.md
@@ -0,0 +1,7 @@
+# Configuration
+you need a config.py file which looks like this:
+username="your username"
+password="your password"
+server="for example xyz.webuntis.com"
+school="your webuntis school name"
+useragent="the useragent you want to use for the api"
--- session.py
+++ session.py
@@ -8,9 +8,6 @@ today = datetime.date.today()
 monday = today - datetime.timedelta(days=today.weekday())
 friday = monday + datetime.timedelta(days=4)
 
-
-
-
 with webuntis.Session(
     username=c.username,
     password=c.password,
@@ -18,4 +15,13 @@ with webuntis.Session(
     school=c.school,
     useragent=c.useragent
 ).login() as s:
-    print("done")
+    table = s.my_timetable(start=monday, end=friday).to_table()
+    print(table, "\n\nTable^")
+    for day in table:
+        print(type(day))
+        print(day)
+        print("\n\n\ndone\n\n\n")
+"""        for period in day[1]:
+            print(type(period))
+            print(period)
+            print("\n\n\ndone\n\n\n")"""
